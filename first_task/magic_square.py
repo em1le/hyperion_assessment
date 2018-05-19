e1f74e0dbdcd0725ccd224b2f165cddbf16c0f71
Emile <emile.bettini@protonmail.com>
2018-05-19 14:16:59 +0200

    add logic that draws a magic square within draw_square()

--- first_task/magic_square.py
+++ first_task/magic_square.py
@@ -1,6 +1,7 @@
 import numpy
 
-def user_interaction():
+
+def user_input():
     """ Fonction that take care of user input
 
     Check if:
@@ -27,9 +28,37 @@ def user_interaction():
             print("{} is not an integer".format(e))
 
 
+def draw_square(square_edge):
+    """ Return a square (numpy matrix)
+
+    Take the user positive odd integer as argument
+    """
+
+    # We create a square with square_edge as size and set the default value to be int
+    square = numpy.zeros((square_edge, square_edge),dtype=int)
+
+    # square is a "matix" of N list
+    # we use list index to place our first number
+    current_list = 0
+    list_index = square_edge // 2
+
+    for number in range(1, square_edge**2+1, 1):
+        square[current_list, list_index] = number
+
+        update_current_list = (current_list - 1) % square_edge
+        update_list_index = (list_index + 1) % square_edge
+
+        if square[update_current_list, update_list_index]:
+            current_list += 1
+
+        else:
+            current_list = update_current_list
+            list_index = update_list_index
 
 def magic_square():
-    pass
+    """  """
+    square_edge = user_input()
+    return draw_square(square_edge)
 
 
-print(user_interaction())
+print(magic_square())
