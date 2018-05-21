b9fa187484f45fa2f2822848269ea68f9458b2e0
Emile <emile.bettini@protonmail.com>
2018-05-21 12:30:56 +0200

    update the update_filedata functions

--- second_task/tracker/checker/cron.py
+++ second_task/tracker/checker/cron.py
@@ -1,8 +1,10 @@
+import pytz
 from .models import FileData
 from connect.dropbox import make_connection
 
 
 # NB It would be a better option to use a scheduled celery task.
+# TODO add a function thats check for files within subfolders...
 def update_filedata():
     """ Retrieve data from dropbox root folder
 
@@ -13,26 +15,50 @@ def update_filedata():
     Returns:
         list -- return a list of updated files
     """
-    query_filedata = FileData.objects.all().values_list('file_id', flat=True)
+
+    # we save our filedata objects in a list
+    query_file_data = [file for file in FileData.objects.all()]
+
+    # we create a dict with k = file_id and v = modification_date
+    data_dict = {}
+    for file in query_file_data:
+        data_dict[file.file_id] = file.modification_date
+
+    # we create a connection to our dropbox folder
     dbx = make_connection()
 
+    # we set a timezone (we will use it while file.client_modified is naive datetime)
+    utc = pytz.timezone('utc')
+
+    # we create a list for our updated file
     updated_file = []
+
+    # we create a list for new file
+    created_file = []
+
+    # we loop on our files in the root folder
     for file in dbx.files_list_folder('').entries:
 
-        if file.id in query_filedata:
-            # Update the date field and the occurence
-            entry = FileData.objects.get(file_id=file.id)
-            entry.modification_date = file.client_modified
-            entry.occurence += 1
-            entry.save()
-            updated_file.append(entry)
+        # If the file exists
+        if file.id in data_dict:
+
+            # If the file has not the same modification date
+            if file.client_modified.astimezone(utc) != data_dict[file.id]:
+                # We update the current file
+                entry = FileData.objects.get(file_id=file.id)
+                entry.modification_date = file.client_modified
+                entry.occurence += 1
+                entry.save()
+                updated_file.append(entry.name)
 
         else:
             # Create an entry within the database
             new_entry = FileData.objects.create(
                             file_id=file.id,
                             name=file.name,
-                            date=file.client_modified,
+                            modification_date=file.client_modified,
                         )
+            created_file.append(file.name)
 
-        return update_file
+    # we return our list that will be displayed after executing the command
+    return updated_file, created_file
