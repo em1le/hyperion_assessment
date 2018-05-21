a243253d414047665252a6d243bd1829ed47ea65
Emile <emile.bettini@protonmail.com>
2018-05-21 12:31:56 +0200

    add crontab job

--- second_task/tracker/tracker/settings.py
+++ second_task/tracker/tracker/settings.py
@@ -14,6 +14,7 @@ import os
 
 from decouple import config
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'checker',
+    'django_crontab',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django crontab
+CRONJOBS = [
+    ('*/5 * * * *', 'checker.cron.update_filedata')
+]
