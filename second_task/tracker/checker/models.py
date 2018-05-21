314de6bdd1fd47af1183a63fbdf4be7069a670d1
Emile <emile.bettini@protonmail.com>
2018-05-21 11:03:53 +0200

    update checker models

--- second_task/tracker/checker/models.py
+++ second_task/tracker/checker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class FileData(models.Model):
+    """ File data model """
+
+    file_id = models.CharField(primary_key=True, unique=True, max_length=64)
+    name = models.CharField(max_length=255)
+    modification_date = models.DateTimeField()
+    occurence = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
